93e5949d6fac852a7c61c30a2b1a3af18306c772
CP422021-Jakkrit <jakkrit.pra@kkumail.com>
2025-07-02 01:48:10 +0700

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { createApp } from 'vue'
 import App from '@/App.vue'
 import { registerPlugins } from '@core/utils/plugins'
+import { createApp } from 'vue'
+
+
 
 // Styles
 import '@core/scss/template/index.scss'
